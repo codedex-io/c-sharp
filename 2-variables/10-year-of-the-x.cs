5467ee740db9f7d7946970bab9584c7c8c582616
Julien Kris <hi.jules.kris@gmail.com>
2026-03-31 11:20:04 -0400

    Create 10-year-of-the-x.cs

---
+++ 2-variables/10-year-of-the-x.cs
@@ -0,0 +1,21 @@
+// Year of the X 📆
+// Codédex
+
+using System;
+
+class Program
+{
+  static void Main()
+  {
+    Console.WriteLine("What year were you born?");
+    
+    string input = Console.ReadLine();
+    int birthYear = int.Parse(input);
+
+    int cycle = 12;
+
+    int yearsUntilZodiac = (cycle - (birthYear % cycle)) % cycle;
+
+    Console.WriteLine("Your zodiac year comes again in " + yearsUntilZodiac + " year(s)!");
+  }
+}
