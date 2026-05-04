6fbbe4b6bd9557c52755a0310b174416147173af
molaskidata <laura.kraft@skidata.com>
2026-05-04 16:06:22 +0200

    Refactor YearOfTheX program for zodiac calculation
    Refactor the program to prompt for birth year and calculate years until next zodiac year.
    I would do that like this, because the code of you is not working. It makes 2 Warnings and one error.
    
    Hope you see that and merge my changes. <3

--- 2-variables/10-YearOfTheX.cs
+++ 2-variables/10-YearOfTheX.cs
@@ -1,21 +1,14 @@
-// Year of the X 📆
-// Codédex
-
 using System;
 
-class YearOfTheX
+class Program
 {
-  static void Main()
-  {
-    Console.WriteLine("What year were you born?");
-    
-    string input = Console.ReadLine();
-    int birthYear = int.Parse(input);
+    static void Main()
+    {
+        Console.Write("Enter your birth year: ");
+        int birthYear = int.Parse(Console.ReadLine());
 
-    int cycle = 12;
-
-    int yearsUntilZodiac = (cycle - ((currentYear - birthYear) % cycle)) % cycle;
-    
-    Console.WriteLine("Your zodiac year comes again in " + yearsUntilZodiac + " year(s)!");
-  }
+        int yearsPassed = DateTime.Now.Year - birthYear;
+        int yearsUntilNextZodiac = (12 - yearsPassed % 12) % 12;
+        Console.WriteLine(yearsUntilNextZodiac);
+    }
 }
