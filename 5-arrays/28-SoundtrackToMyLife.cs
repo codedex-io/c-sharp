bb2459e4c13c1c0bfdb9682e84be94ea68dd559f
Julien Kris <hi.jules.kris@gmail.com>
2026-06-16 15:31:23 -0400

    Create 28-SoundtrackToMyLife.cs

---
+++ 5-arrays/28-SoundtrackToMyLife.cs
@@ -0,0 +1,28 @@
+// Soundtrack to My Life 🎶
+// Codédex
+
+using System;
+
+class SoundtrackToMyLife
+{
+  static void Main()
+  {
+    string[] songs =
+    {
+      "Drive",
+      "Midnight City",
+      "Someone Like You",
+      "Mr. Brightside",
+      "Heroes"
+    };
+
+    Console.WriteLine("Driving Scene:");
+    Console.WriteLine(songs[0]);
+
+    Console.WriteLine("Sad Scene:");
+    Console.WriteLine(songs[2]);
+
+    Console.WriteLine("Closing Credits:");
+    Console.WriteLine(songs[4]);
+  }
+}
