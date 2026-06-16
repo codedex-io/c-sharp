eae60d4cf650dd897121d8957c717b14d809f768
Julien Kris <hi.jules.kris@gmail.com>
2026-06-16 15:31:21 -0400

    Create 27-ArrayOfSunshine.cs

---
+++ 5-arrays/27-ArrayOfSunshine.cs
@@ -0,0 +1,25 @@
+// Array of Sunshine ☀️
+// Codédex
+
+using System;
+
+class ArrayOfSunshine
+{
+  static void Main()
+  {
+    string[] movies =
+    {
+      "Jaws",
+      "Jurassic Park",
+      "The Parent Trap",
+      "Barbie"
+    };
+
+    int[] ratings = { 97, 91, 87, 88 };
+
+    Console.WriteLine(movies[0] + " - " + ratings[0]);
+    Console.WriteLine(movies[1] + " - " + ratings[1]);
+    Console.WriteLine(movies[2] + " - " + ratings[2]);
+    Console.WriteLine(movies[3] + " - " + ratings[3]);
+  }
+}
