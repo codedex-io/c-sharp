903d802f916cbdc87b363d51cd270c3360382d0d
Julien Kris <hi.jules.kris@gmail.com>
2026-06-16 15:31:29 -0400

    Create 32-EmotionalSupportPlushie.cs

---
+++ 5-arrays/32-EmotionalSupportPlushie.cs
@@ -0,0 +1,25 @@
+// Emotional Support Plushie 🧸
+// Codédex
+
+using System;
+
+class EmotionalSupportPlushie
+{
+  static void Main()
+  {
+    string[] toys =
+    {
+      "Teddy Bear",
+      "Action Figure",
+      "Toy Train",
+      "Stuffed Dinosaur",
+      "Rubber Duck",
+      "Yo-Yo"
+    };
+
+    for (int i = 0; i < toys.Length; i++)
+    {
+      Console.WriteLine(toys[i]);
+    }
+  }
+}
