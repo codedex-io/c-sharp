9e491ccdc9cd4dad83334d386a5b04ba438581f5
Julien Kris <hi.jules.kris@gmail.com>
2026-06-16 15:31:27 -0400

    Create 31-NotificationOverload.cs

---
+++ 5-arrays/31-NotificationOverload.cs
@@ -0,0 +1,24 @@
+// Notification Overload 📱
+// Codédex
+
+using System;
+
+class NotificationOverload
+{
+  static void Main()
+  {
+    string[] messages =
+    {
+      "where are you???",
+      "check the group chat",
+      "we're ordering food",
+      "bring snacks",
+      "see you soon!"
+    };
+
+    for (int i = 0; i < messages.Length; i++)
+    {
+      Console.WriteLine(messages[i]);
+    }
+  }
+}
