aaaf73637a9dd89676afc4d1b15298e990affd5d
Julien Kris <65314071+jules-kris@users.noreply.github.com>
2026-09-25 14:25:25 -0400

    Create 41-DreamJob.cs

---
+++ 7-classes-and-objects/41-DreamJob.cs
@@ -0,0 +1,18 @@
+// Dream Job 👩‍🚀
+// Codédex
+
+using System;
+
+class DreamJob
+{
+  public string title;
+  public string description;
+
+  static void Main()
+  {
+    Job myJob = new Job();
+
+    myJob.title = "Pop Star";
+    myJob.description = "perform Oops I Did It Again for millions of adoring fans";
+  }
+}
