38b9b46c7fd61592945ebd81af0f19e2b753c3b0
Julien Kris <65314071+jules-kris@users.noreply.github.com>
2026-09-25 14:25:28 -0400

    Create 43-ApartmentCafe.cs

---
+++ 7-classes-and-objects/43-ApartmentCafe.cs
@@ -0,0 +1,21 @@
+// Apartment Cafe 🍵
+// Codédex
+
+using System;
+
+class ApartmentCafe
+{
+  public string name;
+  public int temperature;
+
+  public Drink(string name, string temperature)
+  {
+    this.name = name;
+    this.temperature = temperature;
+  }
+
+  static void Main()
+  {
+    
+  }
+}
